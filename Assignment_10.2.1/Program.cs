da23f1fd87658d42f0a29e61b72462247b115b97
flowmellow <michael.floresmmt@gmail.com>
2024-07-08 16:20:26 -0400

    Add project files.

---
+++ Assignment_10.2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+1. Write a program in C# Sharp to find the +ve numbers from a list of numbers using where conditions in LINQ Query.
+Input: { 2, -1, 3, -3, 10, -200}
+Expected output: {2, 3, 10}
+*/
+
+namespace Assignment_10._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, -1, 3, -3, 10, -200 };
+
+            var numQuery = from num in nums where num >= 0 select num;
+
+            foreach (int num in numQuery)
+            {
+                Console.Write(num + ",");
+            }
+        }
+    }
+}
